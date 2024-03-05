150b11c293e5f26529b4f3acc6965bf787659e67
Matheus Henrique Schumliak <matheeus.hs@gmail.com>
2024-03-05 19:53:38 -0300

    Criação do projeto via comando NPM e remoção de alguns arquivos desnecessários.

--- Criando Projeto Vite/src/main.jsx
+++ Criando Projeto Vite/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
+
+//recebe o elemento HTML pai, que está no arquivo index.html e faz encadeiamento do componente com ele
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Componente react, que é o arquivo App.jsx  */}
     <App />
   </React.StrictMode>,
 )
